1e967f3b0dbfb5075dc17166b725a1771b50deec
RendyCahyaE <rendycahya6@gmail.com>
2024-04-23 22:48:08 +0700

    done all without liked videos

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -16,7 +16,10 @@ import EmptyState from '../../components/EmptyState';
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard';
+import { useGlobalContext } from '../../context/GlobalProvider';
+
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPost } = useAppwrite(getLatestPosts);
   const [refreshing, setRefreshing] = useState(false);
@@ -41,10 +44,10 @@ const Home = () => {
             <View className="justify-between items-center flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  JSMastery
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
